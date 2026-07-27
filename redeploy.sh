d11cb8ec976c978d3ba063403fbf557993eac0b8
Greg Pfountz <greg@pfountz.com>
2026-07-27 11:44:55 -0400

    script for redeploying

---
+++ redeploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+python3 -m venv .build-venv
+.build-venv/bin/python -m pip install --upgrade pip
+.build-venv/bin/python -m pip wheel --no-deps . --wheel-dir dist
+APP_PATH="$HOME/Applications/Conways Game of Life.app"
+"$APP_PATH/Contents/Resources/venv/bin/python" -m pip install "$PWD"/dist/conways_game_of_life-*.whl --force-reinstall
+rm -rf .build-venv
